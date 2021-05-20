b16b2b613690c90a3e5aa89273d68497145b7e9f
edgardo.nvf <edgardo.nvf@gmail.com>
2021-05-20 16:26:17 -0500

    OCR imagenes

---
+++ OCR/computer/ComputerClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+
+namespace OCR.computer
+{
+    class ComputerClient
+    {
+        private static readonly string APIKEY = "c335ea0982e543719821285136e137f4";
+        private static readonly string ENDPOINT = "https://computerenvef.cognitiveservices.azure.com/";
+        public static ComputerVisionClient Cliente { get; private set; }
+
+        static ComputerClient() { InitComputer(); }
+
+        private static void InitComputer() {
+            var credenciales = new ApiKeyServiceClientCredentials(APIKEY);
+            Cliente = new ComputerVisionClient(credenciales) { Endpoint=ENDPOINT};
+        }
+
+        
+    }
+}
